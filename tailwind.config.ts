b1c767e4e98c215e944cda1d1a071154276c6ea4
Igor <igordonatti.id@gmail.com>
2024-08-14 21:30:40 -0400

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      "yellow-from-figma": {
+        500: "#EBCF6E", 
+      }
+    }
   },
   plugins: [],
 };
